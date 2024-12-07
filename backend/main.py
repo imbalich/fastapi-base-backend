b392e90672af9157ffadb047f00e2eb7006f497a
imbalich <597987319@qq.com>
2024-12-07 17:05:00 +0800

    增加app，增加脚本启动方法

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,6 @@ from backend.core.registrar import register_app
 
 app = register_app()
 
-
 if __name__ == '__main__':
     # 如果你喜欢在 IDE 中进行 DEBUG，main 启动方法会很有帮助
     # 如果你喜欢通过 print 方式进行调试，建议使用 fastapi cli 方式启动服务
